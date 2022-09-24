1eaeb4452fad766270d70363e9212e84c5ab00f4
yiga <igabudalah@gmail.com>
2022-09-24 10:35:02 -0700

    data_structure

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy_list = my_list[:]
+    if idx < 0 or idx >= len(cpy_list):
+        return cpy_list
+    cpy_list[idx] = element
+    return cpy_list
